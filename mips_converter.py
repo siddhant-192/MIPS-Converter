97f621421c2de7113a2b17464d1ca7b4ff628dc1
siddhant-192 <risingphoenix1491@tutanota.com>
2022-09-24 21:17:38 +0530

    reading file lines

--- mips_converter.py
+++ mips_converter.py
@@ -1,13 +1,8 @@
 data = open('data.txt', 'r')
-  
-while True:
-    # Get next line from file
-    line = data.readline()
-  
-    # if line is empty
-    # end of file is reached
-    if not line:
-        break
-    print(line.strip())
-  
+lines=data.readlines()
 data.close()
+
+for cmd in lines:
+    cmd = cmd.replace(',','')
+    cmd = cmd.replace('$','')
+    print(cmd)
